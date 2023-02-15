4f8e42b525dadc2c226853b16e504d770b37691b
Troels Bjerre Lund <troels@google.com>
2023-02-15 22:51:30 +0100

    Initial commit

---
+++ app/src/main/java/handin2/Model.java
@@ -0,0 +1,36 @@
+package handin2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javafx.geometry.Point2D;
+
+public class Model {
+    List<String> lines = new ArrayList<String>();
+    public Model(String filename) {
+        File f = new File(filename);
+        try {
+            try (Scanner s = new Scanner(f)) {
+                while (s.hasNext()) {
+                    lines.add(s.nextLine());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    public void add(Point2D lastmodel, Point2D newmodel) {
+        String s = "LINES".concat(" ").concat(lastmodel.getX()+"").concat(" ")
+        .concat(lastmodel.getY()+"").concat(" ")
+        .concat(newmodel.getX()+"").concat(" ")
+        .concat(newmodel.getY()+"");
+        lines.add(s);
+
+    }   
+
+}
---
+++ app/src/main/java/handin2/View.java
@@ -0,0 +1,82 @@
+package handin2;
+
+import java.util.Iterator;
+
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
+import javafx.stage.Stage;
+
+public class View {
+Canvas canvas = new Canvas(640,480);
+GraphicsContext gc = canvas.getGraphicsContext2D();
+double x1 = 100;
+double y1 = 100;
+double x2 = 200;
+double y2 = 800;
+
+Affine trans = new Affine();
+
+Model model;
+    public View(Model model, Stage primaryStage) {
+        this.model = model;
+        primaryStage.setTitle("Draw Lines");
+        BorderPane pane = new BorderPane(canvas);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        redraw();
+        }
+    void redraw(){
+        Iterator it = model.lines.iterator();
+        gc.setTransform(new Affine());   
+        
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.setTransform(trans);  
+        while (it.hasNext()) {
+            String fileline = (String) it.next();
+            String[] coord = fileline.split(" ");
+            double x1 = Double.parseDouble(coord[1]);
+            double y1 = Double.parseDouble(coord[2]);
+            double x2 = Double.parseDouble(coord[3]);
+            double y2 = Double.parseDouble(coord[4]);
+            gc.beginPath();
+            gc.setLineWidth(0.2);
+               
+            gc.moveTo(x1, y1);
+            gc.lineTo(x2, y2);
+            gc.stroke();
+    }        
+        
+    }
+    void pan(double dx, double dy){
+        trans.prependTranslation(dx, dy);
+        redraw();
+    }
+    void zoom(double dx, double dy, double factor){
+        pan(-dx, -dy);
+        trans.prependScale(factor, factor);
+        pan(dx,dy);
+        redraw();
+    }
+    public Point2D mousetoModel(double lastX, double lastY) {
+        try {
+            return trans.inverseTransform(lastX, lastY);
+        } catch (NonInvertibleTransformException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException(e);
+        }
+        
+    }
+}   
+
+
+
+
